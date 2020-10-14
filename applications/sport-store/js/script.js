5532a62dff17ea8c534a6216f55b12d4d3dcfead
ledung1974 <ledung1974@gmail.com>
2020-10-14 03:20:58 -0600

    Modal Form for SignIn/Create Account using CSS and Javascript

--- applications/sport-store/js/script.js
+++ applications/sport-store/js/script.js
@@ -1,20 +1,39 @@
 function SignIn() {
-    var retVal = confirm("Have you already had an account ?");
-    if( retVal == true ) {
-       var username = prompt("Username: ", "your username here");
-       var password = prompt("Password: ");
-       var x = document.getElementsByClassName("signin");
-       x[0].innerHTML="Customer: "+username;
-       return true;
-    } else {
-        alert ("You should sign up now !");
-       return false;
-    }
- }
-//Stop and Move the marquee
+   var y = document.getElementsByClassName("signin");
+   if (y[0].innerHTML=="Sign In/Create Account") {
+      document.getElementById("div-modal-container").style.display="block";
+      document.getElementById("input-username").value="";
+      document.getElementById("input-password").value="";  
+   }
+   else{
+         if (confirm("Do you want to log out now?.")) {
+            y[0].innerHTML="Sign In/Create Account";
+         } 
+   }
+}
+function submitLoginForm(){
+   var x=document.getElementById("signin_form");
+   var name=x.elements[0].value;
+   var pass=x.elements[1].value;
+   var y=document.getElementsByClassName("signin");
+   if (name !="" & pass!="" ){
+      y[0].innerHTML=name; 
+      document.getElementById("div-modal-container").style.display="none";
+   }
+   else{
+      alert("You must enter username and password")
+   }
+}
+
+function closeLoginForm(){
+   document.getElementById("div-modal-container").style.display="none";
+}
+
+ //Stop and Move the marquee
  function pause() {
     document.getElementById("marquee-news").stop();  
  }
  function move() {
    document.getElementById("marquee-news").start();  
 }
+
