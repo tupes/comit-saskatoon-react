40f744506b299ed82329767fb06b10b03c7f4927
ledung1974 <ledung1974@gmail.com>
2020-10-13 19:18:18 -0600

    Using document.getElementsByClassName("className")

--- applications/sport-store/js/script.js
+++ applications/sport-store/js/script.js
@@ -3,6 +3,8 @@ function SignIn() {
     if( retVal == true ) {
        var username = prompt("Username: ", "your username here");
        var password = prompt("Password: ");
+       var x = document.getElementsByClassName("signin");
+       x[0].innerHTML="Customer: "+username;
        return true;
     } else {
         alert ("You should sign up now !");
