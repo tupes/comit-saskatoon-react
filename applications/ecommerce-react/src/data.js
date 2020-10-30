7d4b75d15a0eaf9b1231fc568b45d4f7e1a00dae
tupala <mark.tupala@resolver.com>
2020-10-29 19:10:47 -0600

    Added data module with items

---
+++ applications/ecommerce-react/src/data.js
@@ -0,0 +1,34 @@
+export function getItems() {
+	return [
+		{
+				name: 'Soccer ball',
+				price: 44.00,
+				description: 'The best soccer ball ever!',
+				image: "imgs/soccer/ball.png",
+		},
+		{
+				name: 'Tubing raft',
+				price: 624.00,
+				description: 'Have some extreme fun being pulled in this raft!',
+				image: "imgs/soccer/ball.png",
+		},
+		{
+				name: 'Chess board and pieces',
+				price: 499.00,
+				description: 'finest wood',
+				image: "imgs/soccer/ball.png",
+		},
+		{
+			name: 'Team Canada soccer jersey',
+			price: 591.00,
+			description: 'Display your national pride with this authentic Team Canada soccer jersey!',
+			image: "imgs/soccer/shirtCanada.png",
+		},
+		{
+			name: 'Goggles',
+			price: 396.00,
+			description: 'Top-of-the-line goggles for the most extreme of watersports!',
+			image: "imgs/watersports/sunglass.png",
+		},
+	]
+};
