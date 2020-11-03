77e207f0e970b0a194c8d986c51eb9b44e9b4fd4
LiliyaIngul <liliyaingul@gmail.com>
2020-11-03 15:52:59 -0500

    email-client project initial converting to React

---
+++ applications/email-client/email-client-react/src/App.js
@@ -0,0 +1,16 @@
+import Container from './components/Container';
+import Header from './components/Header';
+
+
+
+function App() {
+  return (
+    <>
+     <h1>Email Client Application</h1>
+      <Header />    
+      <Container />      
+    </>
+  );
+}
+
+export default App;
---
+++ applications/email-client/email-client-react/src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header_buttons">
+      <button>new massage</button>
+      <button>new contact</button>
+    </div>
+  );
+};
+
+export default Header;
