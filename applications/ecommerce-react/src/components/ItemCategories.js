6fda73efadad9cade994945630957cbe1843e43f
tupala <mark.tupala@resolver.com>
2020-11-03 01:40:53 -0700

    Formatting

--- applications/ecommerce-react/src/components/ItemCategories.js
+++ applications/ecommerce-react/src/components/ItemCategories.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
 export default function ItemCategories() {
-    return (
-    	<ul className="nav-links">
-        <li>
-          <button><a href="pages/watersports.html">Watersports</a></button>
-        </li>
-        <li>
-          <button><a href="pages/soccer.html">Soccer</a></button>
-        </li>
-        <li>
-          <button><a href="pages/basketball.html">Basketball</a></button>
-        </li>
-        <li>
-          <button><a href="pages/hockey.html">Hockey</a></button>
-        </li>
-        <li>
-          <button><a href="pages/Boardgames.html">Boardgames</a></button>
-        </li>
-      </ul>
-    )
+  return (
+    <ul className="nav-links">
+      <li>
+        <button><a href="pages/watersports.html">Watersports</a></button>
+      </li>
+      <li>
+        <button><a href="pages/soccer.html">Soccer</a></button>
+      </li>
+      <li>
+        <button><a href="pages/basketball.html">Basketball</a></button>
+      </li>
+      <li>
+        <button><a href="pages/hockey.html">Hockey</a></button>
+      </li>
+      <li>
+        <button><a href="pages/Boardgames.html">Boardgames</a></button>
+      </li>
+    </ul>
+  )
 }
