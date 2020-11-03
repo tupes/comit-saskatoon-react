2d9171a61caa577c5f01837621cbd099fc4508ea
tupala <mark.tupala@resolver.com>
2020-11-03 01:42:04 -0700

    Added useState to Header

--- applications/ecommerce-react/src/components/App.js
+++ applications/ecommerce-react/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from './Header';
 import ItemCategories from './ItemCategories';
@@ -8,15 +8,15 @@ import Footer from './Footer';
 import { getItems } from '../data';
 
 export default function App() {
+
 	const sportItems = getItems();
-	const isLoggedIn = true;
 
 	return (
-			<div className="container">
-				<Header isLoggedIn={isLoggedIn} pageTitle={"Sports Store!"} />
-				<ItemCategories />
-				<ItemsList items={sportItems} buttonText={"Add to Cart!"} exampleText={'Hi Surpreet'} />
-				<Footer />
-			</div>
+		<div className="container">
+			<Header pageTitle={"Sports Store!"} />
+			<ItemCategories />
+			<ItemsList items={sportItems} buttonText={"Add to Cart!"} />
+			<Footer />
+		</div>
 	);
 }
--- applications/ecommerce-react/src/components/Header.js
+++ applications/ecommerce-react/src/components/Header.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Header(props) {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [count, setCount] = useState(0);
+
+	const handleOnClick = () => {
+		const newCount = count + 1;
+		setCount(newCount);
+		console.log(`The current click count is ${newCount}`);
+
+		setIsLoggedIn(!isLoggedIn); // just a quicker way of doing the logic below  
+		// if (isLoggedIn) {
+		// 	console.log('Setting isLoggedIn to false');
+		// 	setIsLoggedIn(false);
+		// } else {
+		// 	console.log('Setting isLoggedIn to true');
+		// 	setIsLoggedIn(true);
+		// }
+	}
+
 	return (
 		<header className="page-header">
 			<h1>{props.pageTitle}</h1>
-			<button><a href="pages/signup.html">{props.isLoggedIn ? 'Log out' : 'Log in'}</a></button>
+			<div>
+				<button onClick={handleOnClick}>{isLoggedIn ? 'Log out' : 'Log in'}</button>
+				<span>{count}</span>
+			</div>
 		</header>
 	);
 }
