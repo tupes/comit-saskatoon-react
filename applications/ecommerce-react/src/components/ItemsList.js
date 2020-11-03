d2451401a859153d32fd67cdd7d2925ffc782cad
tupala <mark.tupala@resolver.com>
2020-11-03 01:41:22 -0700

    Converted to object destructuring

--- applications/ecommerce-react/src/components/ItemsList.js
+++ applications/ecommerce-react/src/components/ItemsList.js
@@ -1,24 +1,22 @@
 import React from 'react'
 
 export default function ItemsList(props) {	
-	const sportsItems = props.items;
-	const buttonText = props.buttonText;
+	const { items, buttonText } = props;
 
-	console.log(props.exampleText);
 	return (
-      <section className="items">
-        <ul className="items-list">
-          {sportsItems.map((item) => (
-            <li key={item.name}>
-              <img src={item.image} alt="" />
-              <h3>{item.name}</h3>
-          		<div>${item.price}</div>
-              <p>{item.description}</p>
-              <button className="item">{buttonText}</button>
-            </li>          
-          )
-          )}
-        </ul>
-      </section>
+    <section className="items">
+      <ul className="items-list">
+        {items.map((item) => (
+          <li key={item.name}>
+            <img src={item.image} alt="" />
+            <h3>{item.name}</h3>
+            <div>${item.price}</div>
+            <p>{item.description}</p>
+            <button className="item">{buttonText}</button>
+          </li>          
+        )
+        )}
+      </ul>
+    </section>
     )
 }
