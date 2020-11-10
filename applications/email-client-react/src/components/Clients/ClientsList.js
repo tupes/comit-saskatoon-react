fd06dce90eff84a9ca6267f01c17bbbefcb70e11
LiliyaIngul <liliyaingul@gmail.com>
2020-11-10 00:06:10 -0500

    create components ClientsList, Table, added icons in Header component

---
+++ applications/email-client-react/src/components/Clients/ClientsList.js
@@ -0,0 +1,26 @@
+import { BsPerson } from 'react-icons/bs';
+
+const ClientsList = ({clients}) => {
+  return(
+    <>    
+    <div  className="right_sidebar">
+      {clients.map( (client) => {
+
+        const { id, firstName, lastName} = client;
+        return(
+            <dl key={id}>
+                <dt >
+                <BsPerson className="icon" ></BsPerson>                  
+                    <p>{ firstName} {lastName}</p>
+                </dt>                   
+            </dl>
+            )
+          })
+        }
+    </div>
+    </>
+  )  
+}
+
+export default ClientsList;
+
